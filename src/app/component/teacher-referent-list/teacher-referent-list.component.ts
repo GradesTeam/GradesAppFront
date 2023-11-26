a08f8936da313c46cfecad974ceff1e05f098d62
Javier Prieto <jpojaviprior@gmail.com>
2023-11-26 22:48:06 +0100

    Almost done the part for adding a new Referente Evaluacion

--- src/app/component/teacher-referent-list/teacher-referent-list.component.ts
+++ src/app/component/teacher-referent-list/teacher-referent-list.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, TemplateRef, inject } from '@angular/core';
 import { ReferentsService } from '../../services/referents.service';
 import { AllReferenteDTO, ReferenteListResponse } from '../../models/referente-list.interface';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { POSTReferenteDTO } from '../../models/create-referente.interface';
 
 @Component({
   selector: 'app-teacher-referent-list',
   templateUrl: './teacher-referent-list.component.html',
-  styleUrl: './teacher-referent-list.component.css'
+  styleUrl: './teacher-referent-list.component.css',
+  providers: [NgbModalConfig, NgbModal]
 })
 export class TeacherReferentListComponent implements OnInit{
+
   referenteList!: AllReferenteDTO[];
   referenteInfo !: ReferenteListResponse;
   page:number = 0;
   route: ActivatedRoute = inject(ActivatedRoute);
   asignaturaId: string = '';
+  codRef: string = "";
+  desc: string = '';
 
-  constructor(private referenteService: ReferentsService,  private _sanitizer: DomSanitizer){
+  constructor(private referenteService: ReferentsService, private modalService: NgbModal){
     this.asignaturaId = this.route.snapshot.params['id'];
   }
 
@@ -30,4 +35,21 @@ export class TeacherReferentListComponent implements OnInit{
       this.referenteInfo = answ;
     });
   }
+
+  open(content: TemplateRef<any>) {
+      this.modalService.open(content);
+  }
+
+  toSave(){
+    let newRef: POSTReferenteDTO = new POSTReferenteDTO(this.codRef, this.desc);
+    this.referenteService.createReferente(this.asignaturaId, newRef).subscribe(ans => {
+      if(ans instanceof POSTReferenteDTO){
+        window.location.href = "https://localhost:4200/teacher/subject/"+ this.asignaturaId;
+      }else{
+        console.log("hola mundo")
+      }
+    });
+  }
+
+  
 }
--- src/app/services/referents.service.ts
+++ src/app/services/referents.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { ReferenteListResponse } from '../models/referente-list.interface';
 import { environment } from '../enviroment/enviroment';
 import { Observable } from 'rxjs';
+import { POSTReferenteDTO } from '../models/create-referente.interface';
+import { BadRequestReferenteResponse } from '../models/bad-request-referente.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,15 @@ export class ReferentsService {
   constructor(private http: HttpClient) { }
 
   getReferentesFromAsignatura(id:string, page:number):Observable<ReferenteListResponse>{
-    return this.http.get<ReferenteListResponse>(`http://${environment.apiBaseUrl}asignatura/${id}/referentes?page=${page}`);
+    return this.http.get<ReferenteListResponse>(`http://${environment.apiBaseUrl}teacher/asignatura/${id}/referentes?page=${page}`);
+  }
+  createReferente(id:string, newRef: POSTReferenteDTO):Observable<POSTReferenteDTO|BadRequestReferenteResponse>{
+    return this.http.post<POSTReferenteDTO|BadRequestReferenteResponse>(`http://${environment.apiBaseUrl}teacher/asignatura/${id}/referente`, 
+    {
+      codReferente: newRef.codReferente,
+      descripcion: newRef.descripcion
+    },
+    {}
+    );
   }
 }
